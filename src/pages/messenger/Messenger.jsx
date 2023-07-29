0603dcc28bee44db9a0f75dcf34219b03e870d84
boobalannaresh <bestforevermvi@gmail.com>
2023-07-29 23:47:22 +0530

    socket

--- src/pages/messenger/Messenger.jsx
+++ src/pages/messenger/Messenger.jsx
@@ -21,7 +21,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://localhost:5000");
+    socket.current = io("ws://social-media-clone-socket-io.vercel.app");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
